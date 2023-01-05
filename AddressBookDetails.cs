79ce542ef19cfa78623f0b257c0a81b622ca3367
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-05 16:30:54 +0530

    Address book Details added

---
+++ AddressBookDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookDetails
+    {
+        private string fname;
+        private string lname;
+        private string addr;
+        private string city;
+        private string state;
+        private string zip;
+        private string phnumber;
+        private string email;
+
+
+
+        public AddressBookDetails(string fname, string lname, string addr, string city, string state, string zip, string phnumber, string email)
+        {
+            this.fname = fname;
+            this.lname = lname;
+            this.addr = addr;
+            this.city = city;
+            this.zip = zip;
+            this.phnumber = phnumber;
+            this.email = email;
+        }
+        public string Contacts()
+        {
+
+            return this.fname + " " + this.lname + " " + this.addr + " " + this.city + " " + this.state + " " + this.zip + " " + this.phnumber + " " + this.email;
+        }
+    }
+}
