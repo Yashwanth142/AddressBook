fb9bc11103f665f0b85aa6aed739adb90bc66f01
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-06 12:05:37 +0530

    MultiplePerson added

--- Program.cs
+++ Program.cs
@@ -8,14 +8,43 @@
 
             Console.WriteLine("Welcome to Address Book Program");
 
+            int z = 1;
             AddressBookDetails operations = new AddressBookDetails();
-            operations.addPerson();
-            operations.display();
-            operations.edit();
-            Console.WriteLine("After the Edit");
-            operations.display();
-            operations.delete();
-            
+
+
+            while (z > 0)
+            {
+                Console.WriteLine("Enter a number to do which operation: ");
+                Console.WriteLine("1)Add_Contact\t2)Display_Contacts\t3)Edit_Contact\t4)Delete_Contact\t5)Exit");
+                int r = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (r)
+                {
+                    case 1:
+                        operations.addPerson();
+                        break;
+                    case 2:
+                        operations.display();
+
+                        break;
+
+                    case 3:
+                        operations.edit();
+                        break;
+                    case 4:
+                        operations.delete();
+                        break;
+                    case 5:
+                        z = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid number to do which operation: ");
+                        break;
+
+                }
+            }
+
         }
     }
 }
