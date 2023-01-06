3ed1012965f170b40d52a43e22fc131436def537
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-06 11:55:11 +0530

    Edit Contacts added

--- Program.cs
+++ Program.cs
@@ -2,15 +2,18 @@
 {
     class Program
     {
+        public static List<Person> Personlist = new List<Person>();
         public static void Main(string[] args)
         {
 
             Console.WriteLine("Welcome to Address Book Program");
 
-            Console.WriteLine("enter the number of cantacts you want to add: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            AddressBookArray addressBookArray = new AddressBookArray();
-            addressBookArray.addPerson(n);
+            AddressBookDetails operations = new AddressBookDetails();
+            operations.addPerson();
+            operations.display();
+            operations.edit();
+            Console.WriteLine("After the Edit");
+            operations.display();
         }
     }
 }
