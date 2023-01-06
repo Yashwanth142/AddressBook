cecc574bdc9fb3c1ed2fa34e9c2c2187d52fd089
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-06 12:22:25 +0530

    Multiple Address Book added

--- Program.cs
+++ Program.cs
@@ -8,41 +8,56 @@
 
             Console.WriteLine("Welcome to Address Book Program");
 
-            int z = 1;
+            int z = 1, y = 991, flag = 0;
+            string[] book = new string[10];
+
             AddressBookDetails operations = new AddressBookDetails();
-
-
-            while (z > 0)
+            Console.WriteLine("Enter a number of Address Books you want to add: ");
+            int ne = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < ne; i++)
             {
-                Console.WriteLine("Enter a number to do which operation: ");
-                Console.WriteLine("1)Add_Contact\t2)Display_Contacts\t3)Edit_Contact\t4)Delete_Contact\t5)Exit");
-                int r = Convert.ToInt32(Console.ReadLine());
 
+                String stringNumber = y.ToString();
+                book[i] = "AddressBook" + " " + stringNumber;
+                Console.WriteLine("The Address book unique name is: "+ book[i]);
 
-                switch (r)
+                while (z > 0)
                 {
-                    case 1:
-                        operations.addPerson();
-                        break;
-                    case 2:
-                        operations.display();
+                    Console.WriteLine("Enter a number to do which operation: ");
+                    Console.WriteLine("1)Add_Contact\t2)Display_Contacts\t3)Edit_Contact\t4)Delete_Contact\t5)Exit ");
+                    int r = Convert.ToInt32(Console.ReadLine());
 
-                        break;
 
-                    case 3:
-                        operations.edit();
-                        break;
-                    case 4:
-                        operations.delete();
-                        break;
-                    case 5:
-                        z = 0;
-                        break;
-                    default:
-                        Console.WriteLine("Enter a valid number to do which operation: ");
-                        break;
+                    switch (r)
+                    {
+                        case 1:
+                            operations.addPerson();
+                            break;
+                        case 2:
+                            operations.display();
 
+                            break;
+
+                        case 3:
+                            operations.edit();
+                            break;
+                        case 4:
+                            operations.delete();
+                            break;
+                        case 5:
+                            z = 0;
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid number to do which operation:");
+                            break;
+
+                    }
                 }
+                if (i < ne)
+                {
+                    z = 1;
+                }
+                y++;
             }
 
         }
