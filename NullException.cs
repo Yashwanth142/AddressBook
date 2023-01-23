2147754d0dab37c9809729a37812f5cb7858f124
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-23 15:06:46 +0530

    create Custom exception class

---
+++ NullException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+
+    public class NullException : Exception
+    {
+        public NullException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
