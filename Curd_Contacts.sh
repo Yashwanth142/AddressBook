356b8460e6630151c1d1b3913be8194573ac730e
Yashwanth142 <119283440+Yashwanth142@users.noreply.github.com>
2023-02-09 17:55:50 +0530

    all the api's list for curd opeartions in gitbash

---
+++ Curd_Contacts.sh
@@ -0,0 +1,23 @@
+$ json-server --port 4000 --routes CURD_routes.json --watch Relatives.json
+
+  \{^_^}/ hi!
+
+  Loading Relatives.json
+  Loading CURD_routes.json
+  Done
+
+  Resources
+  http://localhost:4000/Contacts
+
+  Other routes
+  /Contacts/list -> /Contacts
+  /Contacts/get/:id -> /Contacts/:id
+  /Contacts/create -> /Contacts
+  /Contacts/update/:id -> /Contacts/:id
+  /Contacts/delete/:id -> /Contacts/:id
+
+  Home
+  http://localhost:4000
+
+  Type s + enter at any time to create a snapshot of the database
+  Watching...
